98ef1dfb2111a589c2818265b360acb194f3b2a4
James M <72298971+Jamesmayl@users.noreply.github.com>
2022-04-18 20:12:27 +0400

    Add files via upload

--- cl_main.lua
+++ cl_main.lua
@@ -20,6 +20,24 @@ local ESX = nil
 local PlayerData = []
 local pedSpawned = false
 
+Citizen.CreateThread(function) ()
+    while ESX = nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(1)
+    end
     
+    while ESX.GetPlayerData() == nil do
+        Citizen.Wait(10)
+    end
+
+    PlayerData = ESX.GetPlayerData()
+end)
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xplayer)
+    PlayerData = xplayer
+end)
+
+RegisterNetEvent('')
    
   
