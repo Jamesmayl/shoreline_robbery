1061ec5121f79c84b97e54a29a4c31a3d91534c0
James M <72298971+Jamesmayl@users.noreply.github.com>
2022-04-22 02:36:52 +0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,7 @@ resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 server_scripts {
 	"@mysql-async/lib/MySQL.lua",
-	"sv_main.lua",
-	"config.lua"
+	"sv_main.lua"
 }
 
 client_script "cl_main.lua"
